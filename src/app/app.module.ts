0a13af84ad67fe3668e74be92f4fe4f3911f4199
Justin Schwartzenberger <schwarty@gmail.com>
2016-10-09 08:36:39 -0700

    end of video commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SandoAppComponent } from './sando-app.component';
 import { OrderSheetComponent } from './counter/order-sheet/order-sheet.component';
@@ -7,7 +8,8 @@ import { DebugPanelComponent } from './counter/debug-panel/debug-panel.component
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   declarations: [
     SandoAppComponent,
--- src/app/counter/order-sheet/order-sheet.component.ts
+++ src/app/counter/order-sheet/order-sheet.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'order-sheet',
   templateUrl: 'app/counter/order-sheet/order-sheet.component.html',
   styleUrls: ['app/counter/order-sheet/order-sheet.component.css']
 })
-export class OrderSheetComponent {  
-  constructor() { }
+export class OrderSheetComponent {
+  orderSheetForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
+  }
+  
+  private buildForm() {
+    this.orderSheetForm = this.formBuilder.group({});
+  }
 }
